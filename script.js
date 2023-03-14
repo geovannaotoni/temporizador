7bc4c6c60423f61a3b40397d12e814d8ee9f5e59
Geovanna Otoni <geovannaotoni@gmail.com>
2023-03-14 20:30:48 -0300

    javascript

---
+++ script.js
@@ -0,0 +1,41 @@
+const buttonStart = document.getElementById('start');
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minute');
+const second = document.getElementById('second');
+const display = document.getElementById('timer');
+const buttonReset = document.getElementById('reset');
+
+buttonStart.addEventListener('click', () => {
+  let duration = Number(hour.value) * 3600 + Number(minute.value) * 60 + Number(second.value);
+  timer(duration);
+})
+
+const timer = (duration) => {
+  let time = duration;
+
+  let interval = setInterval(() => {
+    // Cálculo de cada parte do cronômetro
+    let hours = Math.floor(time / 3600);
+    let minutes = Math.floor(time / 60 - (hours * 60));
+    let seconds = Math.floor(time % 60);
+
+    // fixa cada tempo com 2 dígitos
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    
+    display.innerHTML = `${hours}:${minutes}:${seconds}`;
+  
+    time -= 1;
+
+    if(time < 0){
+      display.innerHTML = 'Tempo Finalizado';
+      clearInterval(interval);
+    }
+  }, 1000);
+
+};
+
+buttonReset.addEventListener('click', () => {
+  window.location.reload();
+})
