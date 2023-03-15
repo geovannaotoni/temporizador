0d3355ad0c69eb4f3e91c1b702e347d4ee282203
Geovanna Otoni <geovannaotoni@gmail.com>
2023-03-15 08:28:36 -0300

    inclui audio despertador

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const minute = document.getElementById('minute');
 const second = document.getElementById('second');
 const display = document.getElementById('timer');
 const buttonReset = document.getElementById('reset');
+const audio = new Audio('alarm_clock.mp3');
 
 buttonStart.addEventListener('click', () => {
   let duration = Number(hour.value) * 3600 + Number(minute.value) * 60 + Number(second.value);
@@ -32,6 +33,7 @@ const timer = (duration) => {
     if(time < 0){
       display.innerHTML = 'Tempo Finalizado';
       clearInterval(interval);
+      audio.play();
     }
   }, 1000);
 
